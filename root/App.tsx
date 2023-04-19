47b7ed3c76c7813aa288b91774d80e20fcfc7299
apple <>
2023-04-19 13:47:08 +0800

    git push

--- root/App.tsx
+++ root/App.tsx
@@ -1,22 +1,18 @@
 import "react-native-gesture-handler";
 import { RootSiblingParent } from 'react-native-root-siblings';
-import { Animated, LogBox } from "react-native";
+import {  LogBox } from "react-native";
 import { Provider } from "react-redux";
 import { store } from "./store/store";
 import { Drawer } from "./routes/Drawer";
 import React, { useState } from "react";
-export const AnimatedContext = React.createContext(void 0);
 const App = () => {
   LogBox.ignoreAllLogs();
-  const [animatedValue, setAnimatedValue] = useState(new Animated.Value(0));
   return (
-    <AnimatedContext.Provider value={animatedValue}>
-      <RootSiblingParent>
+    <RootSiblingParent>
         <Provider store={store}>
           <Drawer />
         </Provider>
       </RootSiblingParent>
-    </AnimatedContext.Provider>
   )
 };
 
